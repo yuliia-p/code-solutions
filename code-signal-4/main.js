12ff3f9322fcf2ff6d2ae1ef6e32457d529392d7
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-10-13 22:02:45 +0000

    All Longest Strings

---
+++ code-signal-4/main.js
@@ -0,0 +1,19 @@
+function solution(inputArray) {
+  // ["aba", "aa", "ad", "vcd", "aba"]
+  let maxLength = 0;
+  const solutionArray = [];
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i].length >= maxLength) {
+      maxLength = inputArray[i].length;
+    }
+  }
+  for (let x = 0; x < inputArray.length; x++) {
+    if (inputArray[x].length === maxLength) {
+      solutionArray.push(inputArray[x]);
+    }
+  }
+  return solutionArray;
+}
+
+solution(['aba', 'aa', 'ad', 'vcd', 'aba']);
+solution(['onsfnib', 'aokbcwthc', 'jrfcw']);
