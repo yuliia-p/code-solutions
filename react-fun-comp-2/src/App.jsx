ea6d6687acf0a8108fa0abda36650806b8a18e19
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-10-16 04:06:55 +0000

    if statments syntax changed

--- react-fun-comp-2/src/App.jsx
+++ react-fun-comp-2/src/App.jsx
@@ -6,38 +6,31 @@ export default function MyPicCarousel() {
   const [index, setIndex] = useState(0);
 
   function handleClickNext() {
-    if (index === myPeople.length - 1) {
-      setIndex(0);
-    } else {
-      setIndex(index + 1);
-    }
+    clearInterval(intervalId);
+    index === myPeople.length - 1 ? setIndex(0) : setIndex(index + 1);
   }
 
   function handleClickPrev() {
-    if (index === 0) {
-      setIndex(myPeople.length - 1);
-      console.log('myPeople.length', myPeople.length);
-    } else {
-      setIndex(index - 1);
-    }
+    clearInterval(intervalId);
+    index === 0 ? setIndex(myPeople.length - 1) : setIndex(index - 1);
   }
-
+  const intervalId = setInterval(handleClickNext, 3000);
   const person = myPeople[index];
 
   return (
     <div className="container">
       <h1>My sketches</h1>
       <div className="flex">
-        <img
-          src={`../img/${person.name}.png`}
-          alt={person.name}
-        />
+        <img src={`../img/${person.name}.png`} alt={person.name} />
         <p>This is
           <span style={{ color: person.color }} > {person.name}</span>
         </p>
+        <p>
+          ({index + 1} of {myPeople.length})
+        </p>
       </div>
-      <button onClick={handleClickNext}>Back</button>
-      <button onClick={handleClickPrev}>Next</button>
+      <i onClick={handleClickPrev} className="fa fa-thin fa-arrow-left"></i>
+      <i onClick={handleClickNext} className="fa fa-thin fa-arrow-right"></i>
     </div >
   );
 }
