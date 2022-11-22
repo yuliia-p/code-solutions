1afee070e47d30083967a7be8a643f259cf9aa81
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-11-22 22:02:18 +0000

    working on detalied view

---
+++ carousel-multiple-items/src/detailed-view.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+
+// function DetailedView(props) {
+//   const fullPic = props.map(img => {
+//     return (
+//       <div key={img.id}>
+//         <div style={{ padding: 8 }}>
+//           <h1 style={{ color: `${img.color}` }}>{img.name}</h1>
+//           <img src={img.url} alt="Josh" />
+//         </div>
+//       </div>
+//     );
+//   });
+//   return fullPic;
+// }
