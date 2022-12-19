b795d87bcd0aff8f9d6b0e98926e6e8fcd4c1e16
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-12-19 04:36:36 +0000

    Century From Year

--- codewars_15/main.js
+++ codewars_15/main.js
@@ -1,39 +1,13 @@
-/*
-I have a cat and a dog.
-I got them at the same time as kitten/puppy. That was humanYears years ago.
-Return their respective ages now as [humanYears,catYears,dogYears]
-
-NOTES:
-humanYears >= 1
-humanYears are whole numbers only
-
-Cat Years
-15 cat years for first year 1
-+9 cat years for second year 2
-+4 cat years for each year after that
-
-Dog Years
-15 dog years for first year
-+9 dog years for second year
-+5 dog years for each year after that
-*/
-var humanYearsCatYearsDogYears = function (humanYears) {
-  // Your code here!
-  // humanYearsCatYearsDogYears(1), [1,15,15]
-  let catsAge = 15;
-  let dogsAge = 15;
-
-  for (let i = 1; i <= humanYears; i++) {
-    if (i === 2) {
-      catsAge += 9;
-      dogsAge += 9;
-    }
-    if (i >= 3) {
-      catsAge += 4;
-      dogsAge += 5;
-    }
+// 1989 => 20
+// 1654 => 17
+// 1800 => 18
+// 2019 => 21
+// Math.ceil(1905 / 100)
+function century(year) {
+  if (year < 100) {
+    return 1;
   }
-
-  return [humanYears, catsAge, dogsAge];
-};
-humanYearsCatYearsDogYears(2);
+  return Math.ceil(year / 100);
+}
+century(1705);
+century(1601);
