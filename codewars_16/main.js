c3d8fc5bfb64ba88ba89292b478b0d02425f87b6
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-12-20 18:05:36 +0000

    Define a card suit

--- codewars_16/main.js
+++ codewars_16/main.js
@@ -1,15 +1,26 @@
+// function defineSuit(card) {
+//   const result = (card[card.length - 1] === '♣')
+//     ? 'clubs'
+//     : (card[card.length - 1] === '♦')
+//         ? 'diamonds'
+//         : (card[card.length - 1] === '♥')
+//             ? 'hearts'
+//             : (card[card.length - 1] === '♠')
+//                 ? 'spades'
+//                 : 0;
+//   return result;
+// }
+
 function defineSuit(card) {
-  const result = (card[card.length - 1] === '♣')
-    ? 'clubs'
-    : (card[card.length - 1] === '♦')
-        ? 'diamonds'
-        : (card[card.length - 1] === '♥')
-            ? 'hearts'
-            : (card[card.length - 1] === '♠')
-                ? 'spades'
-                : 0;
-  return result;
+  const s = {
+    '♣': 'clubs',
+    '♠': 'spades',
+    '♦': 'diamonds',
+    '♥': 'hearts'
+  };
+  return s[card.charAt(card.length - 1)];
 }
+
 defineSuit('Q♠');
 defineSuit('9♦');
 defineSuit('J♥');
