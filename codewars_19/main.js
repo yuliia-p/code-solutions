d911ec9f95b6b30d02c5812d87e1ed2db43f3197
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-12-22 21:20:59 +0000

    Array plus array

--- codewars_19/main.js
+++ codewars_19/main.js
@@ -9,3 +9,9 @@ function arrayPlusArray(arr1, arr2) {
 }
 arrayPlusArray([-1, -2, -3], [-4, -5, -6]);
 arrayPlusArray([0, 0, 0], [4, 5, 6]);
+
+/*
+function arrayPlusArray(arr1, arr2) {
+  return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
+}
+*/
