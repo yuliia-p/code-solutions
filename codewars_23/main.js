22700794e7c93bc1afa610234ee155c9328b02a2
Yuliia Pchelintseva <junoromanova@gmail.com>
2023-01-09 20:54:58 +0000

    Count Odd Numbers below n

---
+++ codewars_23/main.js
@@ -0,0 +1,12 @@
+function oddCount(n) {
+  //   let count = 0;
+  //   for (let i = n; i > 1; i--) {
+  //     if(i % 2 !== 0) {
+  //       count++;
+  //     }
+  //     }
+  //   return count
+  // }
+  return Math.floor(n / 2);
+}
+oddCount(15023);
