1282714af46cf8b1cc3f78d296e52d6efbc27114
Yuliia Pchelintseva <junoromanova@gmail.com>
2023-02-07 20:47:47 +0000

    useState

---
+++ react-hooks-1/src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+const moods = {
+  happy: '😁',
+  sad: '🙁'
+};
+
+export default function Moods() {
+  const [mood, setMood] = useState(moods.happy);
+  const [color, setColor] = useState('yellow');
+
+  function changeMood() {
+    if (mood === moods.happy) {
+      setMood(moods.sad);
+      setColor('blue');
+    } else {
+      setMood(moods.happy);
+      setColor('yellow');
+    }
+  }
+  return <button onClick={changeMood} style={{
+    backgroundColor: color,
+    padding: '1.5rem 3rem',
+    borderRadius: '15px',
+    fontSize: '2rem'
+
+  }}>
+    I am so {mood}
+  </button>;
+}
