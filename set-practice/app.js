631db6557bd11b76ce4216378b722ab4fa9262e4
Yuliia Pchelintseva <junoromanova@gmail.com>
2023-05-23 14:23:18 -0700

    initail-1

---
+++ set-practice/app.js
@@ -0,0 +1,22 @@
+const meals = new Map();
+
+meals.set('🍎', 'breakfast');
+meals.set('🍕', 'lunch');
+meals.set('🍔', 'dinner');
+meals.set('🍪', 'dessert');
+
+console.log('meals', meals)
+
+//Saturday To Do List
+//8am: Walk the dogs
+//12pm: Lunch
+//3pm: Watch a movie
+
+
+const saturday = new Map();
+
+saturday.set(8, 'Walk the dogs')
+saturday.set(12, 'Lanch')
+saturday.set(3, 'Watch a movie')
+
+console.log('saturday', saturday)
